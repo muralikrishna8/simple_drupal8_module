6485462c634764e902580df7898e073b7efea0d6
Murali Krishna <vmurali1444@gmail.com>
2016-06-29 20:47:28 +0530

    added debug logger :: To see goto -> reports->recentlogs

--- src/Controller/RoarController.php
+++ src/Controller/RoarController.php
@@ -3,6 +3,7 @@
 namespace Drupal\dino_roar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelFactory;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
@@ -10,20 +11,24 @@ use Drupal\dino_roar\Jurassic\RoarGenerator;
 class RoarController extends ControllerBase {
 
   private $roarGenerator;
+  protected $loggerFactory;
 
-  public function __construct(RoarGenerator $roarGenerator) {
+  public function __construct(RoarGenerator $roarGenerator, LoggerChannelFactory $loggerFactory) {
     $this->roarGenerator = $roarGenerator;
+    $this->loggerFactory = $loggerFactory;
   }
 
   public function roar($count) {
     $roar = $this->roarGenerator->getRoar($count);
+    $this->loggerFactory->get("default")->debug($roar);
 
     return new Response($roar);
   }
 
   public static function create(ContainerInterface $container) {
     $roarGenerator = $container->get("dino_roar.roar_generator");
+    $loggerFactory = $container->get("logger.factory");
 
-    return new static($roarGenerator);
+    return new static($roarGenerator, $loggerFactory);
   }
 }
